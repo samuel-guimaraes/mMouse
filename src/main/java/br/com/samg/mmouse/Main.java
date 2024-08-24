e4f09ed7017a6233917d79f0df8fa6f57f050e63
samuel-guimaraes <sam.gustavo.guimaraes@gmail.com>
2024-08-24 17:57:45 -0300

    Adicionado classe prinicipal da aplicação Desktop

---
+++ src/main/java/br/com/samg/mmouse/Main.java
@@ -0,0 +1,11 @@
+package br.com.samg.mmouse;
+
+/**
+ * Classe de execução da aplicação.
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
