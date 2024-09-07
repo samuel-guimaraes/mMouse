23a4d60a3f633b91a0e9a82befd8fd3890ea4688
samuel-guimaraes <sam.gustavo.guimaraes@gmail.com>
2024-09-07 14:33:19 -0300

    Adicionado componente gráfico da aplicação Desktop

---
+++ src/main/java/br/com/samg/mmouse/gui/constants/OptionsToExistDialogConstants.java
@@ -0,0 +1,14 @@
+package br.com.samg.mmouse.gui.constants;
+
+import  br.com.samg.mmouse.gui.OptionsToExistDialog;
+
+/**
+ *
+ * Constantes para o {@link OptionsToExistDialog}.
+ */
+public class OptionsToExistDialogConstants {
+    
+    public static final int CLOSE_OPTION = 1;
+    public static final int BACKGROUND_OPTION = 2;
+    
+}
