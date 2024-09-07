2cbeb5c5061ed16c474da72b7ebcfde6ee7a25c2
samuel-guimaraes <sam.gustavo.guimaraes@gmail.com>
2024-09-07 14:33:19 -0300

    Adicionado componente gráfico da aplicação

--- src/main/java/br/com/samg/mmouse/Main.java
+++ src/main/java/br/com/samg/mmouse/Main.java
@@ -1,11 +1,15 @@
 package br.com.samg.mmouse;
 
+import br.com.samg.mmouse.gui.Application;
+import java.net.UnknownHostException;
+
 /**
  * Classe de execução da aplicação.
- * 
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) throws UnknownHostException {
+        Application mainFrame = new Application();
+        mainFrame.setVisible(true);
     }
 }
